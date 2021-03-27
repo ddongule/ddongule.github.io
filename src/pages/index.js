d21de380a9ca384d6cbe48375b34e147567d8964
Minkyung <mnk918p@gmail.com>
2021-03-27 18:13:24 +0900

    feat: reset blog with gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "../components/navigation/navBar";
+import Main from "../components/main/main";
+
+export default function Home() {
+  return <div>Hello world!</div>;
+}
